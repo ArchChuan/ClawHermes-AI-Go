4452cbeecb8d3d68b421a9d2a7a33be69f64ecab
byteBuilderX <18348792873@163.com>
2026-06-03 11:58:54 +0800

    feat: add postgres pool, redis client, and public schema migration

---
+++ internal/migration/migration.go
@@ -0,0 +1,25 @@
+package migration
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"go.uber.org/zap"
+)
+
+func RunPublicSchema(postgresURL string, sqlDir string, logger *zap.Logger) error {
+	m, err := migrate.New("file://"+sqlDir, postgresURL)
+	if err != nil {
+		return fmt.Errorf("migration: init: %w", err)
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("migration: up: %w", err)
+	}
+
+	logger.Info("public schema migration complete")
+	return nil
+}
---
+++ pkg/redis/redis.go
@@ -0,0 +1,37 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+
+	goredis "github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+type Client struct {
+	client *goredis.Client
+	logger *zap.Logger
+}
+
+func New(ctx context.Context, url string, logger *zap.Logger) (*Client, error) {
+	opts, err := goredis.ParseURL(url)
+	if err != nil {
+		return nil, fmt.Errorf("redis: parse url: %w", err)
+	}
+
+	client := goredis.NewClient(opts)
+	if err := client.Ping(ctx).Err(); err != nil {
+		client.Close()
+		return nil, fmt.Errorf("redis: ping: %w", err)
+	}
+
+	logger.Info("redis connected", zap.String("addr", opts.Addr))
+	return &Client{client: client, logger: logger}, nil
+}
+
+func (c *Client) Client() *goredis.Client { return c.client }
+
+func (c *Client) Close() error {
+	c.logger.Info("redis connection closed")
+	return c.client.Close()
+}
